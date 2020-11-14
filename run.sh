041b17d3f1eb75fc9f6ef324bae2de4244ff2b5a
Emanuele Perrella <emanuele@devais.it>
2020-11-14 09:23:45 +0100

    Upgrading Dockerfile.

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-docker build -t audesome . && docker run audesome:latest
+command=$@
+if [ -z "$command" ]
+then
+      echo "\$command is empty"
+else
+      echo "Building with args $command"
+      docker build -t audesome . && docker run -it audesome:latest sbt run $command
+fi
